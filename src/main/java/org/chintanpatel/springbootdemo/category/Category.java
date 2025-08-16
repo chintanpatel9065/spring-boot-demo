ee3f7c9f048479c8d0dbe588907e5c76a0a3d656
chintanpatel <chintanpatel9065@gmail.com>
2025-08-16 18:35:40 +0530

    Add `Category` entity with JPA annotations

---
+++ src/main/java/org/chintanpatel/springbootdemo/category/Category.java
@@ -0,0 +1,41 @@
+package org.chintanpatel.springbootdemo.category;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false)
+    private Long categoryId;
+
+    @NotEmpty(message = "Please Provide Category")
+    @Column(name = "category_name", nullable = false)
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
